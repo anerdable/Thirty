03f95dd97ba6302af5024705554e7fb7363c515e
Paula Silveira <pasi2645@student.su.se>
2019-06-17 11:45:42 +0200

    add dice

--- app/src/main/java/com/example/thirty/activity/MainActivity.java
+++ app/src/main/java/com/example/thirty/activity/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.thirty.activity;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.example.thirty.R;
+import com.example.thirty.fragment.GameFragment;
+import com.example.thirty.fragment.ResultFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    final Fragment gameFragment = new GameFragment();
+    final Fragment resultFragment = new ResultFragment();
+    final FragmentManager fm = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
